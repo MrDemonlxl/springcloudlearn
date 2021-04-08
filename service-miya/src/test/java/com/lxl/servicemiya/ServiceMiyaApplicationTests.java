c15158602756c362ee9240ab45d9aa4ef98587bf
MrDemonlxl <714416426@qq.com>
2021-04-08 16:53:37 +0800

    21.3.31 服务链路追踪(Spring Cloud Sleuth)

---
+++ service-miya/src/test/java/com/lxl/servicemiya/ServiceMiyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxl.servicemiya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceMiyaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
